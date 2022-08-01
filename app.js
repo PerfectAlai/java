59f58a5d1620390714cc6d0c1dedf505b70e0cce
Semenyoperfect <Alaiperfect@gmail.com>
2022-08-01 16:02:08 +0000

    End of class Monday

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+
+//DOM manipulation
+document.getElementById("first").innerHTML = "my first dom manipulation";
+
+//change color of the text
+document.getElementById("first").style.color = "greenyellow";
+
+//change the background color of the text
+document.getElementById("first").style.backgroundColor = "brown";
+
+//change the font size of the text
+document.getElementById("first").style.fontSize = "70px";
+
+// display text as none
+document.getElementById("first").style.display = "none";
